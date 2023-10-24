76fe18bd75047fff07f1cb09550e763236fbf529
harmeetsohi27 <harmeetsohi27@gmail.com>
2023-10-24 13:37:55 +0530

    fix progressloader build.gradle

--- progressloader/build.gradle.kts
+++ progressloader/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("maven-publish")
 }
 
 android {
@@ -22,11 +21,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true

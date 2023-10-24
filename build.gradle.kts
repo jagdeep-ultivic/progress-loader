59ae05343d17b27ac492328adfa4630473523824
harmeetsohi27 <harmeetsohi27@gmail.com>
2023-10-24 17:23:01 +0530

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 buildscript {
     val agp_version by extra("7.4.0")
     val agp_version1 by extra("8.1.0")
+    val agp_version2 by extra("8.1.2")
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.0" apply false
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
